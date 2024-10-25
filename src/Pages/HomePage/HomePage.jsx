06752f4888cb3509a27d7fd0fd2e965901449548
Whaeedil <baoppyhossen1234@gmail.com>
2024-10-25 22:02:20 +0600

    done

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,23 @@
+import BannerSection from "./BannerSection/BannerSection";
+import EventItems from "./EventItems/EventsItem";
+import Gallery from "./Gallery/Gallery";
+import PricingPlan from "./PricingPlan/PricingPlan";
+import RecentlyEvent from "./RecentEvent/RecentEvent";
+import UpCommingEvents from "./UpCommingEvents/UpCommingEvents";
 
 const HomePage = () => {
     return (
-        <div>
+
+        <div className="container mx-auto px-2">
             <h1 className="text-3xl font-bold underline">This is home</h1>
+            <BannerSection />
+            <EventItems />
+            <Gallery />
+            <PricingPlan />
+            <RecentlyEvent />
+            <UpCommingEvents />
         </div>
+
     );
 };
 
