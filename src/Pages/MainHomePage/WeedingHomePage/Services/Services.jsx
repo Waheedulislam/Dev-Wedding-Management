98cb7178b845f758230ab8887a251bc288c7a303
Mominul <ulhouqemomin39@gmail.com>
2024-10-26 19:56:54 +0600

    Added Event Planing

--- src/Pages/MainHomePage/WeedingHomePage/Services/Services.jsx
+++ src/Pages/MainHomePage/WeedingHomePage/Services/Services.jsx
@@ -1,9 +1,13 @@
+import EventPlaning from "./EventPlaning";
 import ServicesBanner from "./ServicesBanner";
 
 const Services = () => {
   return (
     <div>
       <ServicesBanner />
+      <div className="container mx-auto px-2">
+        <EventPlaning />
+      </div>
     </div>
   );
 };
