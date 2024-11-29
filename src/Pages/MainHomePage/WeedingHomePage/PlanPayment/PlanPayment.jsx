36ab9b6b5d13a7e2919635f4620e92e6c821d095
Whaeedil <baoppyhossen1234@gmail.com>
2024-11-29 21:14:33 +0600

    oky

--- src/Pages/MainHomePage/WeedingHomePage/PlanPayment/PlanPayment.jsx
+++ src/Pages/MainHomePage/WeedingHomePage/PlanPayment/PlanPayment.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 import CancelIcon from "@mui/icons-material/Cancel";
 import API from "../../../../api/api";
+import { AuthContext } from "../../../../Providers/AuthProvider";
 const PlanPayment = () => {
   const [meetLink, setMeetLink] = useState("");
   const [isPaid, setIsPaid] = useState(false);
   const { planId } = useParams();
   console.log(planId);
-  const handlePayment = async () => {
-    // SSLCommerz payment logic
-    const paymentId = "sample_payment_id"; // Placeholder for the payment ID
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
-    // Mock payment verification success
-    const paymentSuccessful = true;
-    if (paymentSuccessful) {
-      setIsPaid(true);
-      fetchMeetLink(paymentId);
+  const handlePayment = async () => {
+    if (!user) {
+      navigate('/login')
+    } else {
+      // SSLCommerz payment logic
+      const paymentId = "sample_payment_id"; // Placeholder for the payment ID
+
+      // Mock payment verification success
+      const paymentSuccessful = true;
+      if (paymentSuccessful) {
+        setIsPaid(true);
+        fetchMeetLink(paymentId);
+      }
     }
   };
   const fetchMeetLink = async (paymentId) => {
@@ -35,53 +43,7 @@ const PlanPayment = () => {
       <div className="bg-white rounded-lg shadow-md p-6  max-w-md   ">
         <h1 className="text-2xl font-bold mb-4 text-center">Service Payment</h1>
 
-        {planId === "BasicPlan" && (
-          <div className="group relative flex flex-col items-center bg-white border border-transparent rounded-3xl shadow-lg overflow-hidden transform transition-all duration-500 hover:scale-105 w-full md:w-80 lg:w-96 p-8 hover:border-pink-500  bg-clip-padding bg-gradient-to-tr from-pink-100 to-transparent">
-            <h3 className="text-2xl font-semibold text-gray-800 mb-4">Basic</h3>
-            <div className="relative p-4 border-2 border-pink-400 rounded-full bg-white shadow-lg mb-4">
-              <div className="flex items-end justify-center space-x-1">
-                <span className="text-4xl font-bold text-pink-500">$</span>
-                <span className="text-5xl font-bold text-gray-800">599</span>
-                <span className="text-xl font-semibold text-gray-700">.99</span>
-              </div>
-            </div>
-            <p className="text-gray-600 mt-4 text-center px-4">
-              Cum sociis natoque penatibus et magnis dis parturient montes.
-            </p>
-            <ul className="mt-6 space-y-2 text-gray-700 text-sm text-left  pl-5 list-none">
-              <li>
-                <CheckCircleOutlineIcon
-                  style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }}
-                />
-                Full-service Wedding Planning And Coordination
-              </li>
-              <li>
-                <CheckCircleOutlineIcon
-                  style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }}
-                />
-                Unlimited Consultations
-              </li>
-              <li>
-                <CheckCircleOutlineIcon
-                  style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }}
-                />
-                Design And Decor Assistance
-              </li>
-              <li>
-                <CancelIcon style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }} />
-                Guest Management Services
-              </li>
-              <li>
-                <CancelIcon style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }} />
-                Vendor Management And Negotiations
-              </li>
-              <li>
-                <CancelIcon style={{ color: "#f4a492", fontSize: "24px", marginRight: "5px" }} />
-                Rehearsal Dinner Coordination
-              </li>
-            </ul>
-          </div>
-        )}
+
         {planId === "BasicPlan" && (
           <div className="group relative flex flex-col items-center bg-white border border-transparent rounded-3xl shadow-lg overflow-hidden transform transition-all duration-500 hover:scale-105 w-full md:w-80 lg:w-96 p-8 hover:border-pink-500  bg-clip-padding bg-gradient-to-tr from-pink-100 to-transparent">
             <h3 className="text-2xl font-semibold text-gray-800 mb-4">Basic</h3>
